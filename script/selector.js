250c438100126268cd739e2d7f19bc69f72834d9
Rotno Chandra Das <rotno050@gmail.com>
2026-02-21 19:37:13 +0600

    Done

--- script/selector.js
+++ script/selector.js
@@ -1,3 +1,52 @@
-const addMoneySection = document.getElementById('addmoney');
-const cashOutSection = document.getElementById('cashout');
+const addMoneySection = document.getElementById('add-money');
+const cashOutSection = document.getElementById('cash-out');
+const transferMoneySection = document.getElementById('transfer-money');
+const billSection = document.getElementById('bill');
+const bonusSection = document.getElementById('bonus');
 
+addMoneySection.classList.add('hidden');
+cashOutSection.classList.add('hidden');
+transferMoneySection.classList.add('hidden');
+billSection.classList.add('hidden');
+bonusSection.classList.add('hidden');
+
+
+document.getElementById('grid-add-money').addEventListener('click', () => {
+    addMoneySection.classList.remove('hidden');
+    cashOutSection.classList.add('hidden');
+    transferMoneySection.classList.add('hidden');
+    billSection.classList.add('hidden');
+    bonusSection.classList.add('hidden');
+});
+
+document.getElementById('grid-cash-out').addEventListener('click', () => {
+    addMoneySection.classList.add('hidden');
+    cashOutSection.classList.remove('hidden');
+    transferMoneySection.classList.add('hidden');
+    billSection.classList.add('hidden');
+    bonusSection.classList.add('hidden');
+});
+
+document.getElementById('grid-transfer-money').addEventListener('click', () => {
+    addMoneySection.classList.add('hidden');
+    cashOutSection.classList.add('hidden');
+    transferMoneySection.classList.remove('hidden');
+    billSection.classList.add('hidden');
+    bonusSection.classList.add('hidden');
+});
+
+document.getElementById('grid-pay-bill').addEventListener('click', () => {
+    addMoneySection.classList.add('hidden');
+    cashOutSection.classList.add('hidden');
+    transferMoneySection.classList.add('hidden');
+    billSection.classList.remove('hidden');
+    bonusSection.classList.add('hidden');
+});
+
+document.getElementById('grid-get-bonus').addEventListener('click', () => {
+    addMoneySection.classList.add('hidden');
+    cashOutSection.classList.add('hidden');
+    transferMoneySection.classList.add('hidden');
+    billSection.classList.add('hidden');
+    bonusSection.classList.remove('hidden');
+});
